401ef4ee4f6d4be17749d1bdf78af5d7205c89c8
ibrahim.dursun <ibrahim.dursun@gmail.com>
2008-08-29 16:47:06 +0000

    changed summary page

--- PageFinished.dfm
+++ PageFinished.dfm
@@ -1,25 +1,46 @@
 inherited FinishedPage: TFinishedPage
   Caption = 'FinishedPage'
+  OnCreate = FormCreate
   PixelsPerInch = 96
   TextHeight = 13
   object Label1: TLabel
-    Left = 22
-    Top = 21
-    Width = 442
-    Height = 58
-    Anchors = [akLeft, akTop, akRight]
+    Left = 8
+    Top = 208
+    Width = 418
+    Height = 34
+    Anchors = [akLeft, akRight, akBottom]
     AutoSize = False
     Caption = 
       'Would you like to save this installation as a script so that you' +
-      ' can '#13#10'automate installation of these packages?'
+      ' can automate installation of these packages?'
+    WordWrap = True
+  end
+  object Label2: TLabel
+    Left = 13
+    Top = 9
+    Width = 48
+    Height = 13
+    Caption = 'Summary:'
   end
   object btnSave: TButton
-    Left = 22
-    Top = 82
+    Left = 432
+    Top = 205
     Width = 60
-    Height = 37
+    Height = 25
+    Anchors = [akRight, akBottom]
     Caption = 'Save'
     TabOrder = 0
     OnClick = btnSaveClick
+    ExplicitLeft = 453
+  end
+  object edtSummary: TMemo
+    Left = 8
+    Top = 28
+    Width = 484
+    Height = 163
+    Anchors = [akLeft, akTop, akRight, akBottom]
+    ReadOnly = True
+    ScrollBars = ssBoth
+    TabOrder = 1
   end
 end
--- PageFinished.pas
+++ PageFinished.pas
@@ -9,15 +9,23 @@ interface
 
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-  Dialogs, PageBase, StdCtrls, gnugettext;
+  Dialogs, PageBase, StdCtrls;
 
 type
   TFinishedPage = class(TWizardPage)
     Label1: TLabel;
     btnSave: TButton;
+    edtSummary: TMemo;
+    Label2: TLabel;
     procedure btnSaveClick(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
   private
-    { Private declarations }
+    function Indented(const line: string):string;
+    procedure AddBaseFolder(const summary: TStringList);
+    procedure AddDelphiVersion(const summary: TStringList);
+    procedure AddPackageList(const summary: TStringList);
+    procedure AddSourcePathList(const summary: TStringList);
+    procedure AddHelpFileList(const summary: TStringList);
   public
     procedure UpdateWizardState; override;
     
@@ -27,10 +35,69 @@ var
   FinishedPage: TFinishedPage;
 
 implementation
-uses ScriptPersister,WizardIntfs;
+uses ScriptPersister,WizardIntfs, PackageInfo, gnugettext;
 {$R *.dfm}
 
 { TFinishedPage }
+
+
+procedure TFinishedPage.AddBaseFolder(const summary: TStringList);
+begin
+   summary.Add(_('Base Folder:'));
+   summary.Add(Indented(fCompilationData.BaseFolder));
+end;
+
+procedure TFinishedPage.AddDelphiVersion(const summary: TStringList);
+begin
+  summary.Add(_('Delphi Version:'));
+  summary.Add(Indented(fCompilationData.Installation.IDEVersionNumberStr));
+end;
+
+procedure TFinishedPage.AddHelpFileList(const summary: TStringList);
+var
+  helpfile: string;
+begin
+ summary.Add(_('Help Files:'));
+ for helpfile in fCompilationData.HelpFiles do
+    summary.Add(Indented(helpfile));
+end;
+
+procedure TFinishedPage.AddPackageList(const summary: TStringList);
+var
+  I: Integer;
+  erroredPackages: TStringList;
+  packageName: String;
+begin
+  summary.Add(_('Package List:'));
+  erroredPackages := TStringList.Create;
+  try
+    for I := 0 to fCompilationData.PackageList.Count - 1 do
+    with fCompilationData.PackageList[i] do
+    begin
+      summary.Add(Indented(FileName));
+      if Status = psError then
+        erroredPackages.Add(FileName);
+    end;
+
+    if erroredPackages.Count > 0 then
+    begin
+      summary.Add(_('However, following packages had some errors:'));
+      for packageName in erroredPackages do
+        summary.Add(Indented(packageName));
+    end;
+  finally
+    erroredPackages.Free;
+  end;
+end;
+
+procedure TFinishedPage.AddSourcePathList(const summary: TStringList);
+var
+  path: string;
+begin
+ summary.Add(_('Source File Paths:'));
+ for path in fCompilationData.SourceFilePaths do
+    summary.Add(Indented(path));
+end;
 
 procedure TFinishedPage.btnSaveClick(Sender: TObject);
 var
@@ -52,11 +119,35 @@ begin
   end;
 end;
 
+procedure TFinishedPage.FormCreate(Sender: TObject);
+var
+  summary: TStringList;
+begin
+  inherited;
+  if fCompilationData = nil then exit;
+  summary := TStringList.Create;
+  try
+    AddBaseFolder(summary);
+    AddDelphiVersion(summary);
+    AddPackageList(summary);
+    AddSourcePathList(summary);
+    AddHelpFileList(summary);
+  finally
+    edtSummary.Lines.Assign(summary);
+    summary.Free;
+  end;
+end;
+
+function TFinishedPage.Indented(const line: string): string;
+begin
+  Result := '  ' + line;
+end;
+
 procedure TFinishedPage.UpdateWizardState;
 begin
   inherited;
   FWizard.SetHeader(_('Finished'));
-  FWizard.SetHeader(_('Installations are completed'));
+  FWizard.SetDescription(_('Installation Summary'));
   with FWizard.GetButton(wbtNext) do
     Caption := _('&Exit');
 
