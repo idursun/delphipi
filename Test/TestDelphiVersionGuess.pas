e280b0bd4e8f3c8b4443e1bfb9eeeec83787df68
ibrahim.dursun <ibrahim.dursun@gmail.com>
2012-03-10 17:13:36 +0200

    improved delphi version guess routines

--- Test/TestDelphiVersionGuess.pas
+++ Test/TestDelphiVersionGuess.pas
@@ -6,19 +6,30 @@ uses TestFramework;
 type
 TestDelphiDelphiVersionGuess = class(TTestCase)
   published
-    procedure Should_distinguish_Delphi6_and_DelphiXE2_package_files;
+    procedure Should_guess_Delphi6;
+    procedure Should_guess_DelphiXE2;
 end;
 
 implementation
 uses Utils;
 { TestDelphiDelphiVersionGuess }
 
-procedure TestDelphiDelphiVersionGuess.Should_distinguish_Delphi6_and_DelphiXE2_package_files;
+procedure TestDelphiDelphiVersionGuess.Should_guess_Delphi6;
 var
   actual: integer;
 begin
-   CheckEquals(DELPHI_VERSION_6, Utils.GuessDelphiVersion('pack60'), 'Should guess version as delphi 6');
-   CheckEquals(DELPHI_XE2, Utils.GuessDelphiVersion('pack160'), 'should guess version as delphi xe2');
+   CheckEquals(DELPHI_VERSION_6, Utils.GuessDelphiVersion('pack60'));
+   CheckEquals(DELPHI_VERSION_6, Utils.GuessDelphiVersion('pack6'));
+   CheckEquals(DELPHI_VERSION_6, Utils.GuessDelphiVersion('packD6'));
+   CheckEquals(DELPHI_VERSION_6, Utils.GuessDelphiVersion('packD60'));
+end;
+
+procedure TestDelphiDelphiVersionGuess.Should_guess_DelphiXE2;
+begin
+   CheckEquals(DELPHI_XE2, Utils.GuessDelphiVersion('pack160'));
+   CheckEquals(DELPHI_XE2, Utils.GuessDelphiVersion('pack16'));
+   CheckEquals(DELPHI_XE2, Utils.GuessDelphiVersion('packD16'));
+   CheckEquals(DELPHI_XE2, Utils.GuessDelphiVersion('packD160'));
 end;
 
 initialization
